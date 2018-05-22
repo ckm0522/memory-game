9638acc49c84a43e4ced78aeced30ade0fcf1e71
coco0309 <mitscoco0413@gmail.com>
2018-05-22 11:14:50 -0700

    upload memory game

--- js/app.js
+++ js/app.js
@@ -10,6 +10,36 @@
  *   - add each card's HTML to the page
  */
 
+
+// cards array //
+let card = document.getElementsByClassName("card");
+let cards = [...card]
+
+// card table
+const deck = document.getElementById("card-table");
+
+// move counter
+let moves = 0;
+let counter = document.querySelector(".moves");
+
+// stars
+const stars = document.querySelectorAll(".fa-star");
+
+// card matched
+let matchedCard = document.getElementsByClassName("match");
+
+// stars 
+ let starsList = document.querySelectorAll(".stars li");
+
+ // close icon
+ let closeicon = document.querySelector(".close");
+
+ // popup1
+ let modal = document.getElementById("popup1")
+
+ // opened cards
+var openedCards = [];
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -25,6 +55,154 @@ function shuffle(array) {
     return array;
 }
 
+// refreshed game
+document.body.onload = startGame();
+
+// start game 
+function startGame(){
+    // shuffle cards
+    cards = shuffle(cards);
+    // clear classs
+    for (var i = 0; i < cards.length; i++){
+        deck.innerHTML = "";
+        [].forEach.call(cards, function(item) {
+            deck.appendChild(item);
+        });
+        cards[i].classList.remove("show", "open", "match", "disabled");
+    }
+    // reset moves
+    moves = 0;
+    counter.innerHTML = moves;
+    // reset rating
+    for (var i= 0; i < stars.length; i++){
+        stars[i].style.color = "#FFD700";
+        stars[i].style.visibility = "visible";
+    }
+}
+
+// open show disabled
+var displayCard = function (){
+    this.classList.toggle("open");
+    this.classList.toggle("show");
+    this.classList.toggle("disabled");
+};
+
+// cards matched or not
+function cardOpen() {
+    openedCards.push(this);
+    var len = openedCards.length;
+    if(len === 2){
+        moveCounter();
+        if(openedCards[0].type === openedCards[1].type){
+            matched();
+        } else {
+            unmatched();
+        }
+    }
+};
+
+// cards matched
+function matched(){
+    openedCards[0].classList.add("match", "disabled");
+    openedCards[1].classList.add("match", "disabled");
+    openedCards[0].classList.remove("show", "open", "no-event");
+    openedCards[1].classList.remove("show", "open", "no-event");
+    openedCards = [];
+}
+
+// cards unmatched
+function unmatched(){
+    openedCards[0].classList.add("unmatched");
+    openedCards[1].classList.add("unmatched");
+    disable();
+    setTimeout(function(){
+        openedCards[0].classList.remove("show", "open", "no-event","unmatched");
+        openedCards[1].classList.remove("show", "open", "no-event","unmatched");
+        enable();
+        openedCards = [];
+    },1100);
+}
+
+// disable cards
+function disable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.add('disabled');
+    });
+}
+
+// enable cards ,disable matched cards
+function enable(){
+    Array.prototype.filter.call(cards, function(card){
+        card.classList.remove('disabled');
+        for(var i = 0; i < matchedCard.length; i++){
+            matchedCard[i].classList.add("disabled");
+        }
+    });
+}
+
+// moves count 
+function moveCounter(){
+    moves++;
+    counter.innerHTML = moves;
+
+    // setting rates based on moves
+    if (moves > 8 && moves < 12){
+        for( i= 0; i < 3; i++){
+            if(i > 1){
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+    else if (moves > 13){
+        for( i= 0; i < 3; i++){
+            if(i > 0){
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+}
+
+    // popup
+function congratulations(){
+    if (matchedCard.length == 16){
+        // congratulations modal
+        modal.classList.add("show");
+
+        // star rating
+        var starRating = document.querySelector(".stars").innerHTML;
+
+        // move, rating
+        document.getElementById("finalMove").innerHTML = moves;
+        document.getElementById("starRating").innerHTML = starRating;
+
+        //close icon
+        closeModal();
+    };
+}
+
+// close icon
+function closeModal(){
+    closeicon.addEventListener("click", function(e){
+        modal.classList.remove("show");
+        startGame();
+    });
+}
+
+
+// play Again 
+function playAgain(){
+    modal.classList.remove("show");
+    startGame();
+}
+
+
+// event listeners to each card
+for (var i = 0; i < cards.length; i++){
+    card = cards[i];
+    card.addEventListener("click", displayCard);
+    card.addEventListener("click", cardOpen);
+    card.addEventListener("click", congratulations);
+};
 
 /*
  * set up the event listener for a card. If a card is clicked:
